bacb34467ae4c08d649f15b7afe82f6477695c98
Oscfon <165289370+Oscfon@users.noreply.github.com>
2026-01-30 10:57:16 +0100

    Copyright

--- topsurf/quad_systems.py
+++ topsurf/quad_systems.py
@@ -1,4 +1,27 @@
+r"""
+Homotopy test on maps
 
+This module introduced three classes: :class:`QuadSystems`, :class:`Geodesic` and :class:`Walk`.
+"""
+# ****************************************************************************
+#  This file is part of topsurf
+#
+#       Copyright (C) 2026 Oscar Fontaine
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# ****************************************************************************
 
 
 from topsurf import OrientedMap
